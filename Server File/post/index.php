b11365bda714417715cbad00bab2fd1677cbbaf1
meghlal <meghlal.khan@gmail.com>
2015-09-30 14:46:53 +0530

    php file change
    Added filter

--- Server File/post/index.php
+++ Server File/post/index.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(0);
 
 $servername = "localhost";
@@ -9,31 +8,19 @@ $dbname = "android";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+$name=trim($_POST['name']);
+$roll=trim($_POST['roll']);
+$class=trim($_POST['class']);
 
-$name=$_POST['name'];
-$roll=$_POST['roll'];
-$class=$_POST['class'];
-
-
+if($name!="" && $roll!="" && $roll!=""){
 $sql = "INSERT INTO `student` VALUES (null,'$name', '$roll', '$class')";
-
 	if ($conn->query($sql) === TRUE) {
-	
-
-$response["devices"] = array();
-
-
-
+		$response["devices"] = array();
         $device["id"] = 1;
         $device["type"] = "JSON";
-
-
         array_push($response["devices"], $device);  
-
-
-    $response["success"] = true;
-    echo json_encode($response);
+		$response["success"] = true;
+		echo json_encode($response);
 	}
-
-
 $conn->close();
+}
