90dca61df71477e8d7e123b03b364099a9d1f7c3
meghlal <meghlal.khan@gmail.com>
2015-09-30 13:47:48 +0530

    Initial Structure
    Initial working post mothod

---
+++ app/src/main/java/com/krbmsoft/volleyplus/MainActivity.java
@@ -0,0 +1,75 @@
+package com.krbmsoft.volleyplus;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.krbmsoft.volleyplus.app.AppController;
+import com.krbmsoft.volleyplus.app.MkRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button submit;
+
+    EditText name;
+    EditText roll;
+    EditText cls;
+
+    ProgressDialog pDialog;
+
+    private String URL_POST = "http://192.168.0.3/android/post/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        submit = (Button) findViewById(R.id.savebtn);
+        name = (EditText) findViewById(R.id.name);
+        roll = (EditText) findViewById(R.id.roll);
+        cls = (EditText) findViewById(R.id.cls);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name.getText().toString());
+                params.put("roll", roll.getText().toString());
+                params.put("class", cls.getText().toString());
+
+                MkRequest jsObjRequest = new MkRequest(Request.Method.POST, URL_POST, params, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Response: ", response.toString());
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError response) {
+                        Log.d("Response: ", response.toString());
+                    }
+                });
+                AppController.getInstance().addToRequestQueue(jsObjRequest);
+
+
+            }
+        });
+    }
+
+}
